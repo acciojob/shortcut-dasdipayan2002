4eff625ed23aee9cb70357e5b40061c2b40750ab
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-17 11:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1 === "" || s2 === "") {
+        return "";
+    }
+	let char1 = s1.charAt(0);
+    let char2 = s2.charAt(0);
+
+	return char1+char2;
 }
 
 // Do not change the code below.
